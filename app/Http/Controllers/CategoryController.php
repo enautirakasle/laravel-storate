34cde627eacf24e769a76e44da114e3b5ac710bc
Eñaut AA <enaut.irakasle@gmail.com>
2024-12-30 14:51:18 +0000

    Categorycontroller eginda baina probatu gabe

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -12,7 +12,8 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        
+        Category::all();
+        return view('categories.index', compact('categories'));
     }
 
     /**
@@ -20,7 +21,7 @@ class CategoryController extends Controller
      */
     public function create()
     {
-        //
+        return view('categories.create');
     }
 
     /**
@@ -28,7 +29,9 @@ class CategoryController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        Category::create($request->all());
+        // return redirect()->route('categories.index')->with('success', 'Category created successfully.');
+        return redirect('categories')->with('success', 'Category created successfully.');
     }
 
     /**
@@ -36,7 +39,7 @@ class CategoryController extends Controller
      */
     public function show(Category $category)
     {
-        //
+        return view('categories.show', compact('category'));
     }
 
     /**
@@ -44,7 +47,7 @@ class CategoryController extends Controller
      */
     public function edit(Category $category)
     {
-        //
+        return view('categories.edit', compact('category'));
     }
 
     /**
@@ -52,7 +55,8 @@ class CategoryController extends Controller
      */
     public function update(Request $request, Category $category)
     {
-        //
+        $category->update($request->all());
+        return redirect('categories')->with('success', 'Category updated successfully.');
     }
 
     /**
@@ -60,6 +64,7 @@ class CategoryController extends Controller
      */
     public function destroy(Category $category)
     {
-        //
+        $category->delete();
+        return redirect('categories')->with('success', 'Category deleted successfully.');
     }
 }
